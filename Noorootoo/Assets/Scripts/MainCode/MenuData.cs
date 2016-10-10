6847252996fe273194d136eac65181a8c54251ac
Emily Ryan <emily.ryan@ad.aie.edu>
2016-10-10 17:40:25 +1100

    Emily End of monday - My folder is the noorootoo one + Dash + Disobedient son +UI elements + Glow for Alexa again
    -Bye bye double jump

---
+++ Noorootoo/Assets/Scripts/MainCode/MenuData.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public struct PlayerData
+{
+	GameObject characterSelected;
+	int skinSelected;
+}
+public class MenuData : MonoBehaviour {
+
+	public PlayerData[] Players = new PlayerData[1];
+	private GameObject[] CharacterList;
+
+	public string levelSelected = "Main Build 2"; // farting goose
+
+	// Use this for initialization
+	void Start ()
+	{
+		DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(levelSelected, LoadSceneMode.Single);
+    }
+
+}
---
+++ Noorootoo/Assets/Scripts/MainCode/endMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endMenu : MonoBehaviour
+{
+
+    public void OnMouseDown()
+    {
+        Application.LoadLevel("MAIN SCENCE 2");
+    }
+
+}
