beb0c0680a453a2c59cc9ad7c7dfde61e65198c9
Emily Ryan <emily.ryan@ad.aie.edu>
2016-10-18 13:50:03 +1100

    (possible)end of tues
    +new pow
    +possible bug fix

---
+++ Chroma-master/Chroma4/Assets/Scripts/MainCode/MenuScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	MenuData md;
+	// Use this for initialization
+	void Start()
+    {
+		GameObject MenuDataObject = Instantiate(Resources.Load("MenuData", typeof(GameObject))) as GameObject;
+		MenuDataObject.name = "MenuData";
+		md = MenuDataObject.GetComponent<MenuData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+	private void LoadLevel(string Level)
+	{
+        Application.LoadLevel("ForestTemple");
+		//SceneManager.LoadScene(Level, LoadSceneMode.Single);
+	}
+
+	public void SinglePlayer(string Level)
+	{
+		//md.Players = new GameObject[1];
+
+		Debug.Log("No SinglePlayer for you.");
+
+		VSMatch(Level);
+	}
+	public void VSMatch(string Level)
+	{
+		//md.Players = new GameObject[2];
+
+		LoadLevel("ForestTemple");
+	}
+	public void GameExit()
+	{
+		Application.Quit();
+	}
+}
